c1c417b14390f8130feed300d4ec49723e3cd22c
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-06-19 21:28:23 -0700

    Write JS for home page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@ import _ from 'lodash';
 import './styles.css';
 import Logo from './images/logo.png';
 
-const image = document.getElementById("heroImage");
+const content = document.getElementById('content');
+
+const heading = document.createElement('h2');
+heading.innerText = "Vancouver's Best Steaks";
+
+const paragraph = document.createElement('p');
+paragraph.textContent = "3-Michelin Stars!";
+
+const sloganDiv = document.createElement('div');
+sloganDiv.setAttribute('id', 'slogan');
+sloganDiv.appendChild(heading);
+sloganDiv.appendChild(paragraph);
+
+const image = document.createElement('img');
+image.setAttribute('id', 'heroImage');
 image.src = Logo;
+image.alt = "Timber Logo";
+
+const contentWrapper = document.createElement('div')
+contentWrapper.setAttribute('id', 'contentWrapper');
+contentWrapper.appendChild(image);
+contentWrapper.appendChild(sloganDiv);
+
+content.appendChild(contentWrapper);
